d6c84211684ba2ac1966e75768e804a502fab868
yeasinpavel <yeasin511436@gmail.com>
2026-01-20 02:00:59 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,7 @@
-const CACHE_NAME = 'hasan-telecom-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open("ht-vault").then(cache=>{
+      return cache.addAll(["./","./index.html"]);
+    })
   );
 });
