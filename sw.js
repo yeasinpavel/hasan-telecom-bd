f09d9fb66aa0af0b4de8f47966e0faae083d2786
yeasinpavel <yeasin511436@gmail.com>
2026-01-19 23:14:42 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,10 +8,10 @@ const urlsToCache = [
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
 ];
 
-self.addEventListener('install', e=>{
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
