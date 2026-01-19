41a8b4709634dad88d3be17bc64a4cbceeeaa97e
yeasinpavel <yeasin511436@gmail.com>
2026-01-19 22:34:08 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'hasan-telecom-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
+});
